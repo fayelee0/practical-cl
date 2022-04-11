e5fefe4b2aba2264c71252436fbf947962560c5d
lifei19 <lifei19@baidu.com>
2022-04-11 17:14:11 +0800

    feat: 07 macros

---
+++ 07.lisp
@@ -0,0 +1,68 @@
+;;;; 07 macros
+
+(if (> 2 3) "Yup" "Nope")
+(if (> 2 3) "Yup")
+(if (> 3 2) "Yup" "Nope")
+
+(fboundp 'when)
+(defmacro _when (condition &rest body)
+  `(if ,condition (progn ,@body)))
+
+(macroexpand-1
+ '(_when (spam-p current-message)
+   (file-in-spam-folder current-message)
+   (update-spam-database current-message)))
+
+(fboundp 'unless)
+(defmacro _unless (condition &rest body)
+  `(if (not ,condition) (progn ,@body)))
+
+(macroexpand-1
+ '(dolist (x '(1 2 3 4 5))
+   (format t ".~a~%" x)))
+
+(dolist (x '(1 2 3 4))
+  (print x))
+
+(dolist (x '(1 2 3 4))
+  (print x)
+  (if (evenp x)
+      (return)))
+
+(dotimes (i 4)
+  (print i))
+
+(dotimes (i 4)
+  (print i)
+  (if (oddp i)
+      (return)))
+
+(dotimes (x 20)
+  (dotimes (y 20)
+    (format t "~3d " (* (1+ x) (1+ y))))
+  (format t "~%"))
+
+
+(do ((n 0 (1+ n))
+     (cur 0 next)
+     (next 1 (+ cur next)))
+    ((= 10 n) cur))
+
+(do ((i 0 (1+ i)))
+    ((>= i 4))
+  (print i))
+
+
+(do ((nums nil)
+     (i 1 (1+ i)))
+    ((> i 10) (nreverse nums))
+  (push i nums))
+
+(loop for i from 1 to 10 collecting i)
+(loop for x from 1 to 10 summing (expt x 2))
+(loop for x across "the quick brown fox jumps over the lazy dog"
+      counting (find x "aeiou"))
+(loop for i below 10
+      and a = 0 then b
+      and b = 1 then (+ b a)
+      finally (return a))
