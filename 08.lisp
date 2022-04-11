1b62e9b29d2a9ac246b9caa6a4b19386da390e70
lifei19 <lifei19@baidu.com>
2022-04-11 18:45:28 +0800

    feat: 08 macros

---
+++ 08.lisp
@@ -0,0 +1,78 @@
+;;;; 08 macros
+
+(fboundp 'primep)
+(defun primep (number)
+  (when (> number 1)
+    (loop for fac from 2 to (isqrt number) never (zerop (mod number fac)))))
+
+(defun next-prime (number)
+  (loop for n from number when (primep n) return n))
+
+(do ((p (next-prime 0) (next-prime (1+ p))))
+    ((> p 19))
+  (format t "~d " p))
+
+(defmacro do-primes (var-and-range &rest body)
+  (let ((var (first var-and-range))
+        (start (second var-and-range))
+        (end (third var-and-range)))
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var))))
+         ((> ,var ,end))
+       ,@body)))
+
+(defmacro do-primes ((var start end) &body body)
+  `(do ((,var (next-prime ,start) (next-prime (1+ ,var))))
+       ((> ,var ,end))
+     ,@body))
+
+(do-primes (p 0 19)
+  (format t "~d "p))
+
+(macroexpand-1
+ '(do-primes (p 0 (random 100))
+   (format t "~d " p)))
+
+;;; sequence variables P
+(defmacro do-primes ((var start end) &body body)
+  `(do ((ending-value ,end)
+        (,var (next-prime ,start) (next-prime (1+ ,var))))
+       ((> ,var ending-value))
+     ,@body))
+
+
+
+(defmacro do-primes ((var start end) &body body)
+  `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+        (ending-value ,end))
+       ((> ,var ending-value))
+     ,@body))
+
+(defmacro do-primes ((var start end) &body body)
+  (let ((ending-value-name (gensym)))
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+          (,ending-value-name ,end))
+         ((> ,var ,ending-value-name))
+       ,@body)))
+
+;; (do-primes (ending-value 0 10)
+;;   (print ending-value))
+
+;; (macroexpand-1
+;;  '(let ((ending-value 0))
+;;    (do-primes (p 0 10)
+;;      (incf ending-value p))
+;;    ending-value))
+
+(fboundp 'with-gensyms)
+(defmacro do-primes ((var start end) &body body)
+  (with-gensyms (ending-value-name)
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+          (,ending-value-name ,end))
+         ((> ,var ,ending-value-name))
+       ,@body)))
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(loop for n in '(a b c) collect `(,n (gensym)))
